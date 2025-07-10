7c47217a176a5a86709c73c638f92dfb20fcfc7b
Awais1359 <itsawais2213406@gmail.com>
2025-07-10 15:32:41 +0500

    landing pages

---
+++ script1.js
@@ -0,0 +1,23 @@
+// Optional: Add button interactivity or animations
+document.querySelector('.get-started').addEventListener('click', () => {
+  alert("Welcome! Let's get started!");
+});
+
+document.querySelector('.watch-video').addEventListener('click', () => {
+  alert("This would normally play a video!");
+});
+document.querySelector('.get-started').addEventListener('click', () => {
+  alert("Let's begin your journey! 🚀");
+});
+document.querySelector('.watch-video').addEventListener('click', () => {
+  alert("Here's your video!");
+});
+
+
+document.querySelector(".read-more").addEventListener("click", () => {
+  alert("Read more clicked!");
+});
+
+document.querySelector(".contact-us").addEventListener("click", () => {
+  alert("Contact us clicked!");
+});
